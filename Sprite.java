16d62f9bc78b1304e7a61723c23cda46a4f1c98d
Justin Li <ml.justin.li@gmail.com>
2016-04-01 22:53:15 -0400

    Added sprite collision detection and also made the setters and getters more compact. Got rid of an obsolete getBounds() function that we previously had at school.

--- Sprite.java
+++ Sprite.java
@@ -1,7 +1,8 @@
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
-
 
 public class Sprite {
 	private int x;
@@ -9,12 +10,14 @@ public class Sprite {
 	private int width;
 	private int height;
 	private Image image;
+	private Rectangle rect;
 	
 	public Sprite(int _x, int _y, String imageName) {
 		x = _x;
 		y = _y;
 		loadImage(imageName);
 		getImageDimensions();
+		rect = new Rectangle (x, y, width, height);
 	}
 	
 	public void loadImage(String imageName) {
@@ -27,35 +30,37 @@ public class Sprite {
 		height = image.getHeight(null);
 	}
 	
-	public Image getImage() {
-		return image;
+	public Image getImage() {return image;}
+	public void setImage(Image img) {image = img;}
+	
+	public int getX() {return x;}
+	public void setX(int _x) {x = _x;}
+	
+	public int getY() {return y;}
+	public void setY(int _y) {y = _y;}
+	
+	public int getWidth() {return width;}
+	public int getHeight() {return height;}
+	
+	public Rectangle getRect() {return rect;}
+	
+	/*
+	 * Detects collision with another sprite
+	 */
+	public boolean collision(Sprite other) {
+		return rect.intersects(other.rect);
 	}
 	
-	public void setImage(Image img) {
-		image = img;
-	}
-	
-	public int getX() {
-		return x;
-	}
-	
-	public void setX(int _x) {
-		x = _x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	public void setY(int _y) {
-		y = _y;
-	}
-	
-	public int getWidth() {
-		return width;
-	}
-	
-	public int getHeight() {
-		return height;
+	/*
+	 * Detect collision with another list of sprites
+	 * (for example, an arraylist keeping track of enemy bullets)
+	 */
+	public boolean collision(ArrayList<Sprite> others) {
+		for (Sprite spr : others) {
+			if (rect.intersects(spr.rect)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
