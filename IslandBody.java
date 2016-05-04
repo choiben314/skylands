bb58131cf508c1c9401cd675ee16926f87746ab6
mljustinli <ml.justin.li@gmail.com>
2016-05-04 14:25:02 -0400

    Worked on drawing the actual island in the right place.

--- IslandBody.java
+++ IslandBody.java
@@ -3,6 +3,12 @@ import java.io.Serializable;
 
 public class IslandBody extends Sprite implements Serializable {
 	private static final long serialVersionUID = -2180544871224239940L;
+	
+//	private static int HEIGHT = 231;
+	private static int RANDOMLY_SUBTRACT = 37;
+	private static int HEIGHT = 231 - RANDOMLY_SUBTRACT;
+	private static int Y_OFFSET = -1 * (300 - HEIGHT);
+	
 	private int health;
 
 	public IslandBody(int x, int y, String imageName) {
@@ -19,12 +25,12 @@ public class IslandBody extends Sprite implements Serializable {
 			// draws from center.
 			g.drawImage(getImage(), getX()
 					- (getWidth() * Island.SCALE_FACTOR / 2), getY()
-					- (getHeight() * Island.SCALE_FACTOR / 2), getWidth()
+					- (getHeight() * Island.SCALE_FACTOR / 2) + Y_OFFSET, getWidth()
 					* Island.SCALE_FACTOR, getHeight() * Island.SCALE_FACTOR,
 					null);
 		} else {
 			g.drawImage(getImage(), getX() - getWidth() / 2, getY()
-					- getHeight() / 2, null);
+					- getHeight() / 2 + Y_OFFSET / Island.SCALE_FACTOR, null);
 			// System.out.println("Blah" + getX() + " " + getY());
 		}
 	}
