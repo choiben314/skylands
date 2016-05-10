8425e5fb75684b58fbf53f0eef9111a9728da784
mljustinli <ml.justin.li@gmail.com>
2016-05-10 12:49:55 -0400

    Now it draws the actual island drawing in the right place, regardless of if the island is a rectangle or a square.

--- Island.java
+++ Island.java
@@ -10,8 +10,9 @@ public class Island implements Serializable {
 	
 	public static int SCALE_FACTOR = 3;
 	public static boolean LARGE = true;
-//	public static int[] FIRST_BUILDING_POS = {-414, -486};
-	public static int[] FIRST_BUILDING_POS = {-399, -471};
+//	public static int[] FIRST_BUILDING_POS = {-399, -471};
+	public static int BODY_HEIGHT = 231 * SCALE_FACTOR;
+	public static int[] FIRST_BUILDING_POS = {-399, -1 * (BODY_HEIGHT/2 + 21)};
 	
 	private IslandBody body;
 	private ArrayList<Building> buildings;
